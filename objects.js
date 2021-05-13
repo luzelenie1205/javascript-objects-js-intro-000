5f0948a803a72d23b9465454ea590b78bf1adb7f
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-13 21:29:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,11 @@
 
 var playlist= { artistName: "songTitle" };
 
-function updatePlaylist(playlist, 'artistName', 'songTitle')
+function updatePlaylist(playlist, artistName, songTitle)
 {
   playlist['Slowdive'] = 'Alison'
   playlist['My Bloody Valentine'] = 'Sometimes'
+  playlist['Phil Ochs']= "Here's to the State of Mississippi"
   return playlist
 }
 
