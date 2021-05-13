c0cbeac342aa0c438ea28af27e8b03b0774ae906
bright-generator-f905 <luzelenie1205@gmail.com>
2021-05-13 21:21:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,15 @@
 
 var playlist= { artistName: "songTitle" };
 
-function updatePlaylist(playlist: 'artistName', 'songTitle')
+function updatePlaylist(playlist, artistName, 'songTitle')
 {
   playlist['Slowdive'] = 'Alison'
   playlist['My Bloody Valentine'] = 'Sometimes'
+  return playlist
+}
+
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
   return playlist
 }
